5363070d6d518cf26606d0f8a62009f6be1d442e
Invis1ble <invisiblexman2010@gmail.com>
2016-10-09 11:05:04 +0300

    Refactored visual test script

--- tests/visual/src/js/setup.js
+++ tests/visual/src/js/setup.js
@@ -8,12 +8,12 @@
             let anchors = [];
 
             for (let j = 1; j <= 10; ++ j) {
-                let itemId = i + '-' + j;
+                let itemId = `${i}-${j}`;
 
-                anchors.push('<a id="item-' + itemId + '" href="#item-' + itemId + '">Item ' + itemId + '</a>');
+                anchors.push(`<a id="item-${itemId}" href="#item-${itemId}">Item ${itemId}</a>`);
             }
 
-            content += '<li id="line' + i + '">' + anchors.join(' ') + '</li>';
+            content += `<li id="line${i}">${anchors.join(' ')}</li>`;
         }
 
         content += '</ul>';
